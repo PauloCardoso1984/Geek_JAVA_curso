ca95c442047b5e9a2740cb922d1784615fc99e37
Paulinho <paulocardosoassessor@gmail.com>
2024-05-08 20:27:31 -0300

    Aula_41_93 - CONJUNTOS (MAP / HASHMAP *** LEGAL

--- Aula_41_93_HashMap/src/hashmap/Conta.java
+++ Aula_41_93_HashMap/src/hashmap/Conta.java
@@ -1,13 +1,11 @@
 package hashmap;
 
-import java.util.Objects;
-
 public class Conta {
 	private int numero;
 	private float saldo;
 	private float limite;
-	private Cliente cliente; // Esse atributo só é possivel porque existe a classe cliente
-	
+	private Cliente cliente; // Esse atributo só é possivel porque existe a classe Cliente
+
 	// CONSTRUTOR COM ARGUMENTOS
 	public Conta(int numero, float saldo, float limite, Cliente cliente) {
 		this.numero = numero;
@@ -15,10 +13,10 @@ public class Conta {
 		this.limite = limite;
 		this.cliente = cliente;
 	}
-	
+
 	public Conta() {
 	}
-	
+
 	// Get e Set
 	public int getNumero() {
 		return numero;
@@ -48,26 +46,26 @@ public class Conta {
 	// SAQUE
 	void sacar(float valor) {
 		// REGRA DE NEGÓCIO
-		if(valor <= this.saldo) {
+		if (valor <= this.saldo) {
 			this.saldo = this.saldo - valor;
-		}
-		else if (valor <= this.saldo + this.limite) {
+		} else if (valor <= this.saldo + this.limite) {
 			// CALCULANDO O VALOR EXCEDENTE DO SAQUE
 			float val_ret = this.saldo - valor;
-			if(val_ret < 0) {
+			if (val_ret < 0) {
 				this.saldo = 0;
 			}
 			val_ret = this.limite + val_ret;
 			this.limite = val_ret;
-		}else {	
+		} else {
 			System.out.println("Saldo insuficiente");
 		}
 	}
+
 	// DEPÓSITO
 	void depositar(float valor) {
 		this.saldo = this.saldo + valor;
 	}
-	
+
 	// RETORNAR O SALDO
 	public float getSaldo() {
 		return this.saldo + this.limite;
@@ -75,10 +73,10 @@ public class Conta {
 
 	@Override
 	public String toString() {
-		return "Conta = " + numero + ", saldo = " + saldo + ", limite = " + limite +  ", " + cliente;
+		return "Conta = " + numero + ", saldo = " + saldo + ", limite = " + limite + ", " + cliente;
 	}
 
-	// Equals 
+	// Equals
 
 	@Override
 	public boolean equals(Object obj) {
@@ -90,5 +88,3 @@ public class Conta {
 		}
 	}
 }
-	
-
