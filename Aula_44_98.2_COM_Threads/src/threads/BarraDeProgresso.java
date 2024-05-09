1d93e12b87ebf8c2e8f1038475001c889d4f6dcc
Paulinho <paulocardosoassessor@gmail.com>
2024-05-09 19:11:36 -0300

    Aula_44_98.2 - COM THREADS *** LEGAL

---
+++ Aula_44_98.2_COM_Threads/src/threads/BarraDeProgresso.java
@@ -0,0 +1,21 @@
+package threads;
+
+/*
+ Implementado o método Runnable que faz o Thread funcionar.
+ 
+Implementado o método Runnable que faz o Thread funcionar. 
+Para executar métodos em paralelo, nossa classe deve 
+implementar a interface runnable, e o método que precisa ser 
+executado em paralelo deve ser executado dentro do métoro RUN
+*/ 
+public class BarraDeProgresso implements Runnable {
+
+	@Override
+	// MÉTODO ABSTRATO
+	public void run() {
+		// Fazer um For de número de 0 até 10 mil 
+		for (int i = 0; i < 1000; i ++) {
+			System.out.println(i + " - Barra de Progresso... aguarde ");
+		}
+	}
+}
